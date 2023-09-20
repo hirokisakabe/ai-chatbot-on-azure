6413c14d0a374c95ad07a32ef1bfa0dfb15648c9
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2023-09-20 17:50:45 +0900

    Azure App Serviceを構築
    imageは仮としてnginxを使用
    
    参考
    
    - https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/app_service

--- infra/main.tf
+++ infra/main.tf
@@ -6,3 +6,27 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = random_pet.rg_name.id
 }
+
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = "ai-chatbot-on-azure-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind = "Linux"
+  reserved = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "app_service" {
+  name                = "ai-chatbot-on-azure"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+
+  site_config {
+   linux_fx_version = "DOCKER|registry.hub.docker.com/library/nginx:latest"
+  }
+}
