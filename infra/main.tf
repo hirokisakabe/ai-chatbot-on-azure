dca0412ea4eb693a8563df117f5f3bb41ccc9d42
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2023-09-20 20:03:37 +0900

    Azure Container Registryを構築

--- infra/main.tf
+++ infra/main.tf
@@ -28,3 +28,11 @@ resource "azurerm_linux_web_app" "linux_web_app" {
     }
   }
 }
+
+resource "azurerm_container_registry" "acr" {
+  name                = "aichatbotonazureregistry"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+  admin_enabled       = true
+}
