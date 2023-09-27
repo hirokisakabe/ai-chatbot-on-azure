039a6ee239fd094e435789edc13f2e6077ae4195
Hiroki SAKABE <hiroki.sakabe@icloud.com>
2023-09-27 11:45:30 +0900

    不要なapp_settingsを削除
    site_config.application_stackで定義しているので不要

--- infra/modules/linux_web_app/main.tf
+++ infra/modules/linux_web_app/main.tf
@@ -14,18 +14,15 @@ resource "azurerm_linux_web_app" "linux_web_app" {
   }
 
   app_settings = {
-    WEBSITES_PORT                   = 3000
-    DOCKER_REGISTRY_SERVER_URL      = var.container_registry_url
-    DOCKER_REGISTRY_SERVER_USERNAME = var.container_registry_admin_username
-    DOCKER_REGISTRY_SERVER_PASSWORD = var.container_registry_admin_password
-    NEXTAUTH_SECRET                 = var.NEXTAUTH_SECRET
-    NEXTAUTH_URL                    = var.NEXTAUTH_URL
-    AUTH_GITHUB_ID                  = var.AUTH_GITHUB_ID
-    AUTH_GITHUB_SECRET              = var.AUTH_GITHUB_SECRET
-    REDIS_HOST                      = var.redis_host
-    REDIS_PORT                      = var.redis_port
-    REDIS_PASSWORD                  = var.redis_password
-    OPENAI_API_KEY                  = var.OPENAI_API_KEY
+    WEBSITES_PORT      = 3000
+    NEXTAUTH_SECRET    = var.NEXTAUTH_SECRET
+    NEXTAUTH_URL       = var.NEXTAUTH_URL
+    AUTH_GITHUB_ID     = var.AUTH_GITHUB_ID
+    AUTH_GITHUB_SECRET = var.AUTH_GITHUB_SECRET
+    REDIS_HOST         = var.redis_host
+    REDIS_PORT         = var.redis_port
+    REDIS_PASSWORD     = var.redis_password
+    OPENAI_API_KEY     = var.OPENAI_API_KEY
   }
 
   identity {
